eb35601420887bb1c13b63cff31f06987c62985c
Jorge Castrejon <7059764+jorgecastrejon@users.noreply.github.com>
2018-04-22 19:56:52 +0200

    Adding progress dialog

--- app/src/main/java/com/jcastrejon/filmlibrary/ui/views/base/BaseActivity.kt
+++ app/src/main/java/com/jcastrejon/filmlibrary/ui/views/base/BaseActivity.kt
@@ -1,8 +1,10 @@
 package com.jcastrejon.filmlibrary.ui.views.base
 
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import com.jcastrejon.filmlibrary.R
 import com.jcastrejon.filmlibrary.ui.contracts.base.BaseContract
 
 /**
@@ -12,6 +14,7 @@ internal abstract class BaseActivity : AppCompatActivity(), BaseContract.View {
 
     abstract val layoutId: Int
     abstract val toolbarView: Toolbar
+    private val progress: AlertDialog by lazy { createProgressDialog() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,10 +30,17 @@ internal abstract class BaseActivity : AppCompatActivity(), BaseContract.View {
     open fun update() {}
 
     override fun showProgress() {
-
+        progress.show()
     }
 
     override fun hideProgress() {
-
+        progress.dismiss()
     }
+
+    private fun createProgressDialog() =
+        AlertDialog.Builder(this)
+                .setView(layoutInflater.inflate(R.layout.progress_layout, null))
+                .setCancelable(false)
+                .create()
+                .apply { window.decorView.setBackgroundResource(android.R.color.transparent) }
 }
